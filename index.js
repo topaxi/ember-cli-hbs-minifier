ac8ebb5dcfeddb3bab4b3dc0420ebee9c0a7b617
Damian Senn <senn@puzzle.ch>
2016-06-16 17:13:43 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 /* jshint node: true */
-'use strict';
+'use strict'
+
+const HbsMinifierFilter = require('./lib/hbs-minifier')
 
 module.exports = {
-  name: 'ember-cli-hbs-minifier'
-};
+  name: 'ember-cli-hbs-minifier',
+
+  included: function(app) {
+    this.app = app
+    this.options = app.options.minifyHbs || {}
+  },
+
+  setupPreprocessorRegistry: function(type, registry) {
+    registry.add('template', {
+      name: 'hbs-minifier',
+      ext: 'hbs',
+      toTree: function(tree) {
+        return new HbsMinifierFilter(tree, this.options)
+      }
+    })
+
+    if (registry.remove) {
+      registry.remove('template', 'ember-hbs-minifier')
+    }
+  }
+}
