cb1dfbfea69145b125f7a1bd8772110806d2b40a
Damian Senn <senn@puzzle.ch>
2016-12-05 17:06:29 +0100

    Properly pass through options to filter.
    Fixes #1

--- index.js
+++ index.js
@@ -7,16 +7,26 @@ module.exports = {
   name: 'ember-cli-hbs-minifier',
 
   included: function(app) {
+    this._super.included.apply(this, arguments)
+
     this.app = app
-    this.options = app.options.minifyHbs || {}
+  },
+
+  minifyOptions: function() {
+    let app     = this.app
+    let options = app && app.options && app.options.minifyHbs || {}
+
+    return options
   },
 
   setupPreprocessorRegistry: function(type, registry) {
+    let getOptions = this.minifyOptions.bind(this)
+
     registry.add('template', {
       name: 'hbs-minifier',
       ext: 'hbs',
       toTree: function(tree) {
-        return new HbsMinifierFilter(tree, this.options)
+        return new HbsMinifierFilter(tree, getOptions())
       }
     })
 
